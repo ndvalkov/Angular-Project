f7541f02689e0151c5786967f464ed488c2387af
nicke <ndvalkov@abv.bg>
2017-10-22 17:52:43 +0300

    Scroll top on route change

--- MyCuisine/src/app/app.component.ts
+++ MyCuisine/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
 import {LoaderComponent} from './shared/loader/loader.component';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,15 @@ import {LoaderComponent} from './shared/loader/loader.component';
 export class AppComponent implements OnInit, AfterContentInit {
   @ViewChild('loader') loader: LoaderComponent;
 
+  constructor(private router: Router) { }
+
   ngOnInit(): void {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
   }
 
   ngAfterContentInit(): void {
